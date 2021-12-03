2ebaf135e9deabe7a8909a3fe3ac0420a8a8f8bc
guilherme <guilherme.gl1997@gmail.com>
2021-12-02 22:40:29 -0300

    Create to-do functions

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,30 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const data: Task = {
+      id: Math.random() * (9999 - 1) + 1,
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    if (!newTaskTitle) {
+      return;
+    };
+
+    setTasks([...tasks, data]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const index = tasks.findIndex((task) => task.id === id)
+    tasks[index].isComplete = !tasks[index].isComplete
+    setTasks([...tasks]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
